a28b51d4d0692094e0f00472ec6c3cef3289b4de
Mlab Root <mazerj2006@gmail.com>
2013-05-07 15:52:10 +0000

    - tdtgetraw: notch, lowpass and highpass filters are cached in   ~/.pyperc first time they're generated to speed things up
    git-svn-id: svn+ssh://svn/auto/share/repos/unispike/trunk@107 ce5315e0-74ee-46ee-91fa-aa2f04b5331a

--- tdtgetraw.m
+++ tdtgetraw.m
@@ -2,9 +2,15 @@ function [lfp, spk, wideband] = tdtgetraw(index, ch, want)
 %
 % returns two row vectors -- first row is TIME, second is VOLTAGE
 %
+% Tue May  7 11:41:27 2013 mazer -- Setup to cache filters
+%   in ~/.pyperc. Cache filenames include fs etc, so if a key
+%   param changes, they will get regenerated -- no effort is
+%   make to remove unused filters.
+%
 
 persistent n60 lp hp
 
+notchfreq = 60;
 lfpcut = 200;
 spikecut = 5000;
 
@@ -20,32 +26,56 @@ ix = find(index.icode == icode('RAW0') & index.channel == ch);
 fs = index.frequency(ix(1));
 
 if isempty(n60)
-  fprintf('[making notch filter (fs=%.0f)', fs);
-  % Npoles NotchFreq Qual
-  n60 = design(fdesign.notch(4, 60, 10, fs));
-  fprintf(']\n');
+  cfile = sprintf('%s/.pyperc/.notch-%d-%.0f.mat', ...
+                  getenv('HOME'), notchfreq, fs);
+  if exist(cfile, 'file')
+    load(cfile);
+    %fprintf('[restored notch filter (fs=%.0f)]\n', fs);
+  else
+    fprintf('[making notch filter (fs=%.0f)', fs);
+    % Npoles NotchFreq Qual
+    n60 = design(fdesign.notch(4, notchfreq, 10, fs));
+    fprintf(']\n');
+    save(cfile, 'n60');
+  end    
 end
 
 if isempty(lp) && any(want=='l')
-  fprintf('[making lfp filter (fs=%.0f)', fs);
-  % Fpass Fstop Apass Astop
-  lp = design(fdesign.lowpass(lfpcut, 2*lfpcut, 0.1, 25, fs));
-  fprintf(']\n');
+  cfile = sprintf('%s/.pyperc/.lfpcut-%d-%.0f.mat', ...
+                  getenv('HOME'), lfpcut, fs);
+  if exist(cfile, 'file')
+    load(cfile);
+    %fprintf('[restored lfpcut filter (fs=%.0f)]\n', fs);
+  else
+    fprintf('[making lfp filter (fs=%.0f)', fs);
+    % Fpass Fstop Apass Astop
+    lp = design(fdesign.lowpass(lfpcut, 2*lfpcut, 0.1, 25, fs));
+    fprintf(']\n');
+    save(cfile, 'lp');
+  end
 end
 
 if isempty(hp) && any(want=='s')
-  fprintf('[making spike filter (fs=%.0f)', fs);
-  % default for design is an equiripple filter, which takes
-  % ~20s just to compute. Kaiser looks ok -- longer filter,
-  % and more group delay, but we're going to filter with
-  % filtfilt, which should eliminate the (flat) group
-  % delay...
-  %
-  % Fstop1 Fpass1 Fpass2 Fstop2 Astop1 Apass1 Astop2:
-  hp = design(fdesign.bandpass(lfpcut/2, lfpcut, ...
-                               spikecut, 2*spikecut, 25, 0.1, 25, fs), ...
-              'kaiserwin');
-  fprintf(']\n');
+  cfile = sprintf('%s/.pyperc/.hpcut-%d-%d-%.0f.mat', ...
+                  getenv('HOME'), lfpcut, spikecut, fs);
+  if exist(cfile, 'file')
+    load(cfile);
+    %fprintf('[restored spike filter (fs=%.0f)]\n', fs);
+  else
+    fprintf('[making spike filter (fs=%.0f)', fs);
+    % default for design is an equiripple filter, which takes
+    % ~20s just to compute. Kaiser looks ok -- longer filter,
+    % and more group delay, but we're going to filter with
+    % filtfilt, which should eliminate the (flat) group
+    % delay...
+    %
+    % Fstop1 Fpass1 Fpass2 Fstop2 Astop1 Apass1 Astop2:
+    hp = design(fdesign.bandpass(lfpcut/2, lfpcut, ...
+                                 spikecut, 2*spikecut, 25, 0.1, 25, fs), ...
+                'kaiserwin');
+    fprintf(']\n');
+    save(cfile, 'hp');
+  end
 end
 
 d = tdtraw(index, ix);
